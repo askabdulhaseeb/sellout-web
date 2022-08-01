bdd41fda6dc3bab5a11148e3a7c0bbe587a3bca2
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-08-01 20:01:15 +0500

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'providers/auction_provider.dart';
 import 'providers/auth_state_provider.dart';
 import 'providers/app_provider.dart';
@@ -10,7 +12,11 @@ import 'providers/product_category_provider.dart';
 import 'providers/user_provider.dart';
 import 'route.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
