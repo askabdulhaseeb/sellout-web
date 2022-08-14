86bf2bc9661f4fe5e68738e7c91d833e288fe49c
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-08-14 13:22:55 +0500

    Update route.dart

--- lib/route.dart
+++ lib/route.dart
@@ -27,41 +27,46 @@ final GoRouter router = GoRouter(
     // Dashboard
     //
     GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) => const Dashboard(),
-    ),
-    GoRoute(
-      path: '/${HomePage.routeName}',
-      builder: (BuildContext context, GoRouterState state) => const HomePage(),
-      routes: [
-        GoRoute(
-            name: ProductDetailScreen.routeName,
-            path: ':id',
-            builder: (BuildContext context, GoRouterState state) {
-              final Product _prod = Provider.of<ProdProvider>(context)
-                  .product(state.params['id'] ?? '');
-              return ProductDetailScreen(product: _prod);
-            }),
-      ],
-    ),
-    GoRoute(
-      path: '/${BitPage.routeName}',
-      builder: (BuildContext context, GoRouterState state) => const BitPage(),
-    ),
-    GoRoute(
-      path: '/${AddProductPage.routeName}',
-      builder: (BuildContext context, GoRouterState state) =>
-          const AddProductPage(),
-    ),
-    GoRoute(
-      path: '/${ChatPage.routeName}',
-      builder: (BuildContext context, GoRouterState state) => const ChatPage(),
-    ),
-    GoRoute(
-      path: '/${ProfilePage.routeName}',
-      builder: (BuildContext context, GoRouterState state) =>
-          const ProfilePage(),
-    ),
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const Dashboard(),
+        routes: [
+          GoRoute(
+            path: ':${HomePage.routeName}',
+            builder: (BuildContext context, GoRouterState state) =>
+                const HomePage(),
+            routes: [
+              GoRoute(
+                  name: ProductDetailScreen.routeName,
+                  path: ':id',
+                  builder: (BuildContext context, GoRouterState state) {
+                    final Product _prod = Provider.of<ProdProvider>(context)
+                        .product(state.params['id'] ?? '');
+                    return ProductDetailScreen(product: _prod);
+                  }),
+            ],
+          ),
+          GoRoute(
+            path: ':${BitPage.routeName}',
+            builder: (BuildContext context, GoRouterState state) =>
+                const BitPage(),
+          ),
+          GoRoute(
+            path: ':${AddProductPage.routeName}',
+            builder: (BuildContext context, GoRouterState state) =>
+                const AddProductPage(),
+          ),
+          GoRoute(
+            path: ':${ChatPage.routeName}',
+            builder: (BuildContext context, GoRouterState state) =>
+                const ChatPage(),
+          ),
+          GoRoute(
+            path: ':${ProfilePage.routeName}',
+            builder: (BuildContext context, GoRouterState state) =>
+                const ProfilePage(),
+          ),
+        ]),
   ],
   errorBuilder: (BuildContext context, GoRouterState state) =>
       const ErrorPage(),
